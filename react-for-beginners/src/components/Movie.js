1d837c4609a63d322dc50eaf951def1562956338
Seungho Lee <60536427+dltmdgh1994@users.noreply.github.com>
2022-03-08 17:49:48 +0900

    Update MovieApp

--- react-for-beginners/src/components/Movie.js
+++ react-for-beginners/src/components/Movie.js
@@ -1,10 +1,13 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-function Movie({ coverImage, title, summary, hasGenres, genres }) {
+function Movie({ id, coverImage, title, summary, hasGenres, genres }) {
   return (
     <div>
       <img src={coverImage} />
-      <h2>{title}</h2>
+      <h2>
+        <Link to={`/movie/${id}`}>{title}</Link>
+      </h2>
       <p>{summary}</p>
       {/* gernes가 없는 경우 에러 => 예외처리 필요 */}
       {hasGenres ? (
@@ -19,6 +22,7 @@ function Movie({ coverImage, title, summary, hasGenres, genres }) {
 }
 
 Movie.propTypes = {
+  id: PropTypes.number.isRequired,
   coverImage: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
--- react-for-beginners/src/routes/Home.js
+++ react-for-beginners/src/routes/Home.js
@@ -1,45 +1,45 @@
 import { useEffect, useState } from "react";
-import Movie from "./components/Movie";
+import Movie from "../components/Movie";
 
-function Home(){
-    const [loading, setLoading] = useState(true);
-    const [movies, setMovies] = useState([]);
-    // 비동기 처리를 위한 async
-    // await 키워드를 사용하면 결과값을 얻을 수 있을 때까지 기다림
-    const getMovies = async () => {
-      const json = await (
-        await fetch(
-          "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
-        )
-      ).json();
-      setMovies(json.data.movies);
-      setLoading(false);
-    };
-    useEffect(() => {
-      getMovies();
-    }, []);
-  
-    return (
-      <div>
-        {loading ? (
-          <h1>Loading...</h1>
-        ) : (
-          <div>
-            {movies.map((movie) => (
-              <Movie
-                key={movie.id}
-                coverImage={movie.medium_cover_image}
-                title={movie.title}
-                summary={movie.summary}
-                hasGenres={movie.hasOwnProperty("genres")}
-                genres={movie.genres}
-              />
-            ))}
-          </div>
-        )}
-      </div>
-    );
-  }
+function Home() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  // 비동기 처리를 위한 async
+  // await 키워드를 사용하면 결과값을 얻을 수 있을 때까지 기다림
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              id={movie.id}
+              coverImage={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              hasGenres={movie.hasOwnProperty("genres")}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Home;
