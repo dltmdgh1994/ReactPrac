5482f36cd659b8b2a229d6693a65d8b0c46481d5
Seungho Lee <60536427+dltmdgh1994@users.noreply.github.com>
2022-03-10 19:46:10 +0900

    Learn styled-components

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -1,33 +1,5 @@
-import styled from "styled-components";
-
-// npm i styled-components
-// vscode-styled-components 익스텐션 필요
-const Father = styled.div`
-  display: flex;
-`;
-const Box1 = styled.div`
-  background-color: teal;
-  width: 100px;
-  height: 100px;
-`;
-const Box2 = styled.div`
-  background-color: tomato;
-  width: 100px;
-  height: 100px;
-`;
-const Text = styled.span`
-  color: white;
-`;
-
 function App() {
-  return (
-    <Father>
-      <Box1>
-        <Text>Hello</Text>
-      </Box1>
-      <Box2 />
-    </Father>
-  );
+  return null;
 }
 
 export default App;
