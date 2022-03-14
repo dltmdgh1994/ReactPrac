18ac66de60bd57cd73cc03f7f0820ff2a1f8252d
Seungho Lee <60536427+dltmdgh1994@users.noreply.github.com>
2022-03-14 20:00:44 +0900

    Update Cointracker

--- coin-tracker/src/routes/Coin.tsx
+++ coin-tracker/src/routes/Coin.tsx
@@ -1,9 +1,52 @@
-import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
+interface ILocation {
+  state: {
+    name: string;
+  };
+}
 
 function Coin() {
+  const [loading, setLoading] = useState(true);
   const { coinId } = useParams();
+  // react-router-dom이 보내주는 location object에 접근
+  const { state } = useLocation() as ILocation;
 
-  return <h1>Coin : {coinId}</h1>;
+  return (
+    <Container>
+      <Header>
+        {/* state가 존재하면 name을 가져오고 아니면 로딩 */}
+        {/* state를 통해 홈화면으로 올때만 정보 접근 가능 */}
+        <Title>{state?.name || "Loading..."}</Title>
+      </Header>
+      {loading ? <Loader>Loading...</Loader> : null}
+    </Container>
+  );
 }
 
 export default Coin;
